999c6ed6068be8ff01cf7130aee2e26fecd16f09
svetlyoek <svetlyo_svetlyo@abv.bg>
2020-02-25 15:40:46 +0200

    Add view component, tag helpers and partials views

---
+++ MyCalisthenicApp/MyCalisthenicApp.Web/Components/SideBarViewComponent.cs
@@ -0,0 +1,23 @@
+﻿namespace MyCalisthenicApp.Web.Components
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [ViewComponent(Name ="SideBar")]
+    public class SideBarViewComponent : ViewComponent
+    {
+        public SideBarViewComponent()
+        {
+            //TODO
+            //Inject service here
+        }
+
+        //TODO Async
+        public IViewComponentResult Invoke()
+        {
+            //TODO View-list from latest n count of posts
+
+            return View();
+        }
+
+    }
+}
